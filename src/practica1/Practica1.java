9a0c75843c3566de49c8b30a9d48e23c6ecb200f
antjim <ant.jim.v@gmail.com>
2017-10-10 19:15:01 +0200

    sleep

--- src/practica1/Practica1.java
+++ src/practica1/Practica1.java
@@ -2,6 +2,7 @@ package practica1;
 
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.TimeUnit;
 
 public class Practica1 {
 
@@ -10,6 +11,12 @@ public class Practica1 {
 		
 		lecturaDirs.leeDirectorios(dirInicial);//Solo se puede ejecutar 1 vez, crea el Hash de todos los archivos de dirInicial
 		System.out.println("================================");
+		try {
+			TimeUnit.SECONDS.sleep(30);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		comprobarDir.comprobarHash(dirInicial, "SHA-256");//¿Como llamar al Init de comprobarHash?
 
 	}
