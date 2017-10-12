c287201eedde908d839077bbee8886430b62a7aa
antjim <ant.jim.v@gmail.com>
2017-10-12 16:03:06 +0200

    version 1 cifrado aes

--- src/practica1/metodos.java
+++ src/practica1/metodos.java
@@ -1,19 +1,13 @@
 package practica1;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.UnsupportedEncodingException;
-import java.security.DigestInputStream;
 import java.security.InvalidKeyException;
-import java.security.Key;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 
@@ -23,9 +17,6 @@ import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.KeyGenerator;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
-import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.DESKeySpec;
-import javax.crypto.spec.SecretKeySpec;
 
 public class metodos {
 	
@@ -45,15 +36,16 @@ public class metodos {
 	public static void cifraAES(File log,File log2) throws NoSuchAlgorithmException, 
 			IllegalBlockSizeException, BadPaddingException, NoSuchPaddingException, InvalidKeyException, IOException, InvalidKeySpecException {
 		
-		//KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
-	    //keyGenerator.init(128);
+		KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+	    SecretKey key=keyGenerator.generateKey();
+		keyGenerator.init(128);
 	    //Key key = keyGenerator.generateKey(); 	//hasta aquí todo de acuerdo
-	   // SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
+	    //SecretKeySpec skeySpec = new SecretKeySpec(key, "AES");
 		
-		InputStreamReader leer_clave = new InputStreamReader(System.in);
+		/*InputStreamReader leer_clave = new InputStreamReader(System.in);
         BufferedReader buff_clave = new BufferedReader(leer_clave);
         System.out.print("Escriba una clave de 16 caracteres: ");
-        String clave = buff_clave.readLine();
+        String clave = buff_clave.readLine();*/
         
        /* InputStreamReader opt = new InputStreamReader(System.in);
         BufferedReader buff_opt = new BufferedReader(opt);
@@ -61,7 +53,7 @@ public class metodos {
         String op = buff_opt.readLine();
         */
 
-	       SecretKeySpec kspec = new SecretKeySpec(clave.getBytes(),0,16,"AES");
+	   //    SecretKeySpec kspec = new SecretKeySpec(key.getBytes(),0,16,"AES");
 //	       SecretKey ks = skf.generateSecret(kspec);
 		
 		
@@ -69,9 +61,9 @@ public class metodos {
 	    
 	    Cipher aes = Cipher.getInstance("AES/ECB/PKCS5Padding");	//correcto
 	    
-	    aes.init(Cipher.ENCRYPT_MODE, kspec);	//modification
+	    aes.init(Cipher.ENCRYPT_MODE, key);	//modification
 	    
-	    //aes.init(Cipher.DECRYPT_MODE, kspec);
+	   // aes.init(Cipher.DECRYPT_MODE, key);
 	   // byte[] desencriptado = aes.doFinal(encriptado);
 	    
 	    //lectura de ficheros----
@@ -88,19 +80,28 @@ public class metodos {
      
         while( leidos != fin_archivo ) {
            bloque_cifrado = aes.update(buffer,0,leidos);
-           textoCifrado = textoCifrado + new String(bloque_cifrado,"ISO-8859-1");
+           textoCifrado = textoCifrado + new String(bloque_cifrado);
            leidos = archivo.read(buffer);         
+           System.out.println("blq: "+bloque_cifrado);
         }
+        
+        
+        System.out.println("texto: "+textoCifrado);
         
         archivo.close();
         
         bloque_cifrado = aes.doFinal();
-        textoCifrado = textoCifrado + new String(bloque_cifrado,"ISO-8859-1");
+        System.out.println("blq2: "+bloque_cifrado);
+        textoCifrado = textoCifrado + new String(bloque_cifrado,"iso-8859-1");
+        
+        
         //ISO-8859-1 es ISO-Latin-1
      
-        fich_out.write(textoCifrado.getBytes("ISO-8859-1"));//escribir fichero
+        fich_out.write(textoCifrado.getBytes("iso-8859-1"));//escribir fichero
      
-        
+       /* aes.init(Cipher.DECRYPT_MODE, key);
+        byte[] desencriptado = aes.doFinal(bloque_cifrado);
+        System.out.println("bloque descifrado: "+desencriptado);*/
         //----------------------
         
         
@@ -116,6 +117,7 @@ public class metodos {
 	    
 	    System.out.println(new String(desencriptado));*/
 	}
+	
 	
 	
 }
--- src/practica1/principal.java
+++ src/practica1/principal.java
@@ -1,15 +1,35 @@
 package practica1;
 
+import java.io.File;
 import java.io.IOException;
+import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 
 public class principal {
 
 	public static void main(String[] args) throws NoSuchAlgorithmException, IOException {
 		String rutaConf="configuracion.txt";
 		List<String>datosFicheros=null;
+		
+		//pruebas de cifrado
+		File log=new File("logs/l.txt");
+		File log2=new File("logs/l2.txt");
+		try {
+			metodos.cifraAES(log, log2);
+		} catch (InvalidKeyException | IllegalBlockSizeException | BadPaddingException | NoSuchPaddingException
+				| InvalidKeySpecException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+				
+		//------------------
 		
 		try {
 			datosFicheros=leerFicheroConfiguracion.leeFichero(rutaConf); //length->4.
